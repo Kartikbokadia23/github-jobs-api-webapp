996022a22be151d3cfcfcf0f18b14412e99cf8d9
kartikbokadia23 <kartikbokadia@gmail.com>
2021-03-17 15:49:12 +0530

    webapp initialize

---
+++ jobView.js
@@ -0,0 +1,36 @@
+const params = new URLSearchParams(window.location.search)
+var id = params.get('id')
+
+getJobData(id)
+
+function getJobData(jobId){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+        getDescription(this.responseText)
+        }
+    }
+    xhttp.open("GET",`https://jobs.github.com/positions/${jobId}.json`,true);
+    xhttp.send();
+}
+
+function getDescription(data){
+    jsonData = JSON.parse(data)
+    console.log(jsonData)
+
+    var str = jsonData.how_to_apply.slice(jsonData.how_to_apply.indexOf('http'),jsonData.how_to_apply.indexOf('">'))
+    console.log(str)
+
+    document.getElementsByTagName('img')[0].src = jsonData.company_logo
+    document.getElementsByClassName('job-view-title')[0].innerHTML = jsonData.company
+    document.getElementsByTagName('a')[0].href = jsonData.company_url
+
+    document.getElementsByClassName('job-title')[0].innerHTML = jsonData.title
+    document.getElementsByClassName('job-type')[0].innerHTML = jsonData.type
+    document.getElementsByClassName('job-location')[0].innerHTML = jsonData.location
+    document.getElementsByTagName('a')[1].href = str
+
+
+    document.getElementsByClassName('description-body')[0].innerHTML += jsonData.description
+    document.getElementsByClassName('description-body')[0].innerHTML += jsonData.how_to_apply
+}
---
+++ main.js
@@ -0,0 +1,86 @@
+var container = document.getElementById("card-body");
+var page = 0;
+
+// window.onload = (function(){
+//     loadfunc()
+// })
+
+document.addEventListener("DOMContentLoaded", function(){
+    loadfunc()
+});
+
+function loadfunc(){
+    var description = document.getElementById("search-description").value.trim()
+    var location = document.getElementById("search-location").value.trim()
+    if(location.includes(" ")){
+        location = location.replace(" ", "+");
+    }
+    var fullTime = document.getElementById("full-time-check").checked
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+        container.innerHTML = "";
+        document.getElementsByClassName('load')[0].style.display = "flex"
+        data_scraper(this.responseText)
+        }
+    }
+    xhttp.open("GET",`https://jobs.github.com/positions.json?description=${description}&location=${location}&full_time=${fullTime}`,true);
+    xhttp.send();
+    
+}
+
+function data_scraper(data){
+    json_data = JSON.parse(data)
+    console.log(json_data.length)
+
+    json_data.forEach(entry => {
+        var card = document.createElement('div');
+        card.classList = 'card';
+        const content = `
+            <p class = "job-type" id = "${entry.id}">${entry.type}</p>
+            <img class = "job-company-logo" src = "${entry.company_logo}">
+            <h3 class = "job-title">${entry.title}</h3>
+            <p class = "job-company">${entry.company}</p>
+            <p class = "job-location">${entry.location}</p>
+        `;
+
+        card.innerHTML = content
+
+        card.addEventListener('click', getId);
+
+        container.append(card) 
+
+    });
+}
+
+function getId(){
+    var jobId = this.getElementsByClassName("job-type")[0].id
+    url = `http://127.0.0.1:5500/jobview.html?id=${jobId}`
+    location.href = url;
+}
+
+
+if(document.getElementById("search-description").value == "" && document.getElementById("search-location").value == ""){
+    document.getElementsByClassName('load')[0].style.display = "none"
+}
+
+
+function load(){
+    page+=1;
+    var description = document.getElementById("search-description").value.trim()
+    var location = document.getElementById("search-location").value.trim()
+    if(location.includes(" ")){
+        location = location.replace(" ", "+");
+    }
+    var fullTime = document.getElementById("full-time-check").checked
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+        data_scraper(this.responseText)
+        }
+    }
+    xhttp.open("GET",`https://jobs.github.com/positions.json?description=${description}&location=${location}&full_time=${fullTime}&page=${page}`,true);
+    xhttp.send();
+}
+
+
